c1c9495e0ee9aabbcc11450b3f0b642eefd436d3
Thiago Martins de Freitas <martins86@gmail.com>
2020-09-12 18:14:05 -0300

    implementando model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from './../models/todo.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   // Cria uma lista todo vazia.
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   // Titulo da tela.
   public title: String = 'Minhas Tarefas';
   
   ngOnInit() {}
   
   constructor() {
-    this.todos.push('Lavar o Carro');
-    this.todos.push('Cortar a Grama');
+    this.todos.push(new Todo(1, 'Lavar o Carro', false));
+    this.todos.push(new Todo(2, 'Cortar a Grama', true));
+    this.todos.push(new Todo(3, 'Cortar o Cabelo', false));
   }
 }
