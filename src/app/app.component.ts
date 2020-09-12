e13f6c7c4b9327f4e39efb500dd19fc3e1815791
Thiago Martins de Freitas <martins86@gmail.com>
2020-09-12 17:53:04 -0300

    listando itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-todo';
+export class AppComponent implements OnInit {
+  // Cria uma lista todo vazia.
+  public todos: any[] = [];
+  // Titulo da tela.
+  public title: String = 'Minhas Tarefas';
+  
+  ngOnInit() {}
+  
+  constructor() {
+    this.todos.push('Lavar o Carro');
+    this.todos.push('Cortar a Grama');
+  }
 }
